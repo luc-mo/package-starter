2b79c014e718337e8e77f3ed8dedcd90be5c6e38
SnowDrive <luc.moralesr@gmail.com>
2024-05-31 07:39:00 -0400

    feat: add config file

---
+++ apps/cli/src/infrastructure/config/index.js
@@ -0,0 +1,7 @@
+export const config = {
+	repoUrl: 'https://github.com/luc-mo/react-package-starter.git',
+	templates: {
+		'js.browser.react': 'templates/react-js',
+		'ts.browser.react': 'templates/react-ts',
+	},
+}
