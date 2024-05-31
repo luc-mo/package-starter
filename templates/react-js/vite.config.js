e5b03b3ec30df4edd87eccd94475eea9ef5c1054
SnowDrive <luc.moralesr@gmail.com>
2024-05-31 07:32:06 -0400

    feat: add react js template

---
+++ templates/react-js/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
+	build: {
+		copyPublicDir: false,
+		lib: {
+			entry: 'src/lib/index.js',
+			fileName: 'index',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			external: ['react'],
+			output: {
+				globals: {
+					react: 'React',
+				},
+				dir: 'dist',
+			},
+		},
+		minify: false,
+	},
+	plugins: [react(), libInjectCss()],
+})
