13f0c83a7595451d6ff798896a1d7bf3b913e04f
SnowDrive <luc.moralesr@gmail.com>
2024-06-02 05:41:33 -0400

    feat: add cli version from package json file

--- apps/cli/src/index.js
+++ apps/cli/src/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import { version } from '../package.json'
 import { program } from 'commander'
 import { GetTemplatePrompt } from './application/get-template-prompt'
 import { CloneTemplate } from './application/clone-template'
@@ -6,10 +7,9 @@ import { CloneTemplate } from './application/clone-template'
 const main = async () => {
 	const getTemplatePrompt = new GetTemplatePrompt()
 	const cloneTemplate = new CloneTemplate()
-
 	const appDir = process.argv[2]
 	const answer = await getTemplatePrompt.execute()
 	await cloneTemplate.execute({ appDir, answer })
 }
 
-program.version('0.0.0-beta.4').action(main).parse(process.argv)
+program.version(version).action(main).parse(process.argv)
