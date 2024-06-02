6bcfa09468d00b61fde2f1d0105dafb2c0a23a93
SnowDrive <luc.moralesr@gmail.com>
2024-06-02 03:05:53 -0400

    feat: add clone template use case

---
+++ apps/cli/src/application/clone-template.js
@@ -0,0 +1,25 @@
+import os from 'node:os'
+import fs from 'node:fs'
+import path from 'node:path'
+import crypto from 'node:crypto'
+import simpleGit from 'simple-git'
+import { config } from '@/infrastructure/config'
+
+export class CloneTemplate {
+	async execute({ appDir, answer }) {
+		const tempId = crypto.randomUUID()
+		const tempDir = path.join(os.tmpdir(), tempId)
+		const templateDir = path.join(tempDir, config.templates[answer])
+
+		try {
+			const git = simpleGit()
+			await git.clone(config.repoUrl, tempDir)
+			fs.cpSync(templateDir, appDir, { recursive: true })
+		} catch (error) {
+			// TODO - create a custom error to handle this
+			console.error(error)
+		} finally {
+			fs.rmSync(tempDir, { recursive: true, force: true })
+		}
+	}
+}
