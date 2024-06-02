b81a50098fd8de9eb1bf908710fe7748a1e38838
SnowDrive <luc.moralesr@gmail.com>
2024-06-02 03:08:57 -0400

    feat: update main function using new use cases, remove get environment and framework prompts

--- apps/cli/src/index.js
+++ apps/cli/src/index.js
@@ -1,42 +1,15 @@
 #!/usr/bin/env node
-
-import os from 'node:os'
-import fs from 'node:fs'
-import path from 'node:path'
-import crypto from 'node:crypto'
-
 import { program } from 'commander'
-import simpleGit from 'simple-git'
-
-import { config } from './infrastructure/config'
-import { Answer } from './domain/answer'
-
-import { GetEnvironment } from './application/get-environment'
-import { GetFramework } from './application/get-framework'
-
-const handlePrompts = async () => {
-	const envAnswers = await GetEnvironment.execute()
-	const fullAnswers = await GetFramework.execute(envAnswers)
-	return new Answer(fullAnswers).toString()
-}
+import { GetTemplatePrompt } from './application/get-template-prompt'
+import { CloneTemplate } from './application/clone-template'
 
 const main = async () => {
+	const getTemplatePrompt = new GetTemplatePrompt()
+	const cloneTemplate = new CloneTemplate()
+
 	const appDir = process.argv[2]
-	const answer = await handlePrompts()
-
-	const repoId = crypto.webcrypto.randomUUID()
-	const tempDir = path.join(os.tmpdir(), repoId)
-	const templateDir = path.join(tempDir, config.templates[answer])
-
-	try {
-		const git = simpleGit()
-		await git.clone(config.repoUrl, tempDir)
-		fs.cpSync(templateDir, appDir, { recursive: true })
-	} catch (error) {
-		console.error(error)
-	} finally {
-		await fs.rmSync(tempDir, { recursive: true, force: true })
-	}
+	const answer = await getTemplatePrompt.execute()
+	await cloneTemplate.execute({ appDir, answer })
 }
 
 program.version('0.0.0-beta.4').action(main).parse(process.argv)
