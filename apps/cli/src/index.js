f86a54684bb64394bc18a6c8da231b94a4a3367a
SnowDrive <luc.moralesr@gmail.com>
2024-06-02 08:58:21 -0400

    feat: apply empty app dir checking to avoid unintentional overwriting

--- apps/cli/src/index.js
+++ apps/cli/src/index.js
@@ -3,15 +3,28 @@ import { version } from '../package.json'
 import { program } from 'commander'
 
 import { config } from './infrastructure/config'
+import { ProcessAbortedError } from './domain/errors'
+
+import { CheckEmptyAppDir } from './application/check-empty-app-dir'
 import { GetTemplatePrompt } from './application/get-template-prompt'
 import { CloneTemplate } from './application/clone-template'
 
 const main = async () => {
-	const getTemplatePrompt = new GetTemplatePrompt()
-	const cloneTemplate = new CloneTemplate()
-	const appDir = process.argv[2] ?? config.defaultName
-	const answer = await getTemplatePrompt.execute()
-	await cloneTemplate.execute({ appDir, answer })
+	try {
+		const checkEmptyAppDir = new CheckEmptyAppDir()
+		const getTemplatePrompt = new GetTemplatePrompt()
+		const cloneTemplate = new CloneTemplate()
+
+		const appDir = process.argv[2] ?? config.defaultName
+		await checkEmptyAppDir.execute({ appDir })
+
+		const answer = await getTemplatePrompt.execute()
+		await cloneTemplate.execute({ appDir, answer })
+	} catch (error) {
+		if (error instanceof ProcessAbortedError) {
+			console.error(error.message)
+		}
+	}
 }
 
 program.version(version).action(main).parse(process.argv)
